68c289a5e3623517c48736920311681d32300e8f
Nesh-Sinha <nsnesh25@gmail.com>
2025-05-08 19:22:55 +0530

    corrected errors

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # This is where your development static files are kept
+]
 
+  
 
 
 # Application definition
